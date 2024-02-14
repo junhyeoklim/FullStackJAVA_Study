99157d5b71eebd10af5b44faf4b2c94cb058d807
junhyeoklim <gur710@gmail.com>
2024-02-14 17:04:10 +0900

    실습 한 내용 추가

---
+++ Day6/실습 문제/PlayMain.java
@@ -0,0 +1,53 @@
+/*1. 어린아이가 소유하고 있는 구슬의 개수 정보
+2. 놀이를 통한 구슬의 주고 받음 -> 메소드
+3. 어린이의 현재 보유 자산을 출력하는 메소드
+
+
+
+
+
+*/
+
+class Child
+{
+	int cnt;
+	String name;
+
+	public Child(String name,int cnt)
+	{
+		this.cnt = cnt;
+		this.name = name;
+	}
+
+	public void win(Child child,int cnt)
+	{
+		this.cnt += child.lose(cnt);
+	}
+
+	public int lose(int cnt)
+	{
+		this.cnt -= cnt;
+		return cnt;
+	}
+
+	public void childStatus()
+	{
+		System.out.println("이름 : "+name);
+		System.out.println("현재 보유중인 구슬의 갯수 : "+cnt);
+	}
+}
+
+class PlayMain
+{
+	public static void main(String[] args)
+	{
+		Child c1 = new Child("어린이1",15);
+		Child c2 = new Child("어린이2",7);
+
+
+		c1.win(c2,2);
+
+		c1.childStatus();
+		c2.childStatus();
+	}
+}
