34e3a0a21f5b3c7d43da8cfbb831a1bdb80352a4
junhyeoklim <gur710@gmail.com>
2024-02-14 14:15:24 +0900

    GetSet사용

---
+++ 혼자 끄적인 것들/Day6/FruitGetSet예제/data/DataGetSet.java
@@ -0,0 +1,32 @@
+package data;
+
+public class DataGetSet {
+
+	private int price,cnt,money;
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public int getCnt() {
+		return cnt;
+	}
+
+	public void setCnt(int cnt) {
+		this.cnt = cnt;
+	}
+
+	public int getMoney() {
+		return money;
+	}
+
+	public void setMoney(int money) {
+		this.money = money;
+	}
+	
+	
+}
