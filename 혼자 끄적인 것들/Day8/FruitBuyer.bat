22ae8faf790782ff5ecfdd5773f4d6c859935ab6
junhyeoklim <gur710@gmail.com>
2024-02-19 10:59:05 +0900

    7일차 배운거 테스트

---
+++ 혼자 끄적인 것들/Day8/FruitBuyer.bat
@@ -0,0 +1,22 @@
+class FruitBuyer
+{
+	int myMoney;
+	int numOfApple;
+	
+	public FruitBuyer(int money)
+	{
+		myMoney=money;
+		numOfApple=0;
+	}
+	
+	public void buyApple(FruitSeller seller, int money)
+	{
+		numOfApple+=seller.saleApple(money);
+		myMoney-=money;
+	}
+	public void showBuyResult()
+	{
+		System.out.println("현재 잔액: " + myMoney);
+		System.out.println("사과 개수: " + numOfApple);		
+	}
+}
---
+++ 혼자 끄적인 것들/Day8/FruitBuyer.java
@@ -0,0 +1,25 @@
+package orange.buyer;
+import orange.seller.FruitSeller;
+
+class FruitBuyer
+{
+	int myMoney;
+	int numOfApple;
+	
+	public FruitBuyer(int money)
+	{
+		myMoney=money;
+		numOfApple=0;
+	}
+	
+	public void buyApple(FruitSeller seller, int money)
+	{
+		numOfApple+=seller.saleApple(money);
+		myMoney-=money;
+	}
+	public void showBuyResult()
+	{
+		System.out.println("현재 잔액: " + myMoney);
+		System.out.println("사과 개수: " + numOfApple);		
+	}
+}
