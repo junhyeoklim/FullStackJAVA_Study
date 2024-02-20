9c626f871b313b7becc804e09dab8b8ba6e37b80
junhyeoklim <gur710@gmail.com>
2024-02-20 18:44:13 +0900

    9일차 혼자 끄적인 것들

---
+++ 혼자 끄적인 것들/Day9/Test.java
@@ -0,0 +1,95 @@
+/*class Test
+{
+	public static void main(String[] args)
+	{
+		int numOfApples = 123;
+		int sizeOfBucket = 10;
+
+		int numOfBucket = numOfApples/sizeOfBucket +(numOfApples%sizeOfBucket > 0 ? 1 : 0);
+
+		System.out.println("필요한 바구니의 수 : "+ numOfBucket);
+	}
+}
+
+class Test
+{
+	public static void main(String[] args)
+	{
+		int num = 10;
+		System.out.println(num > 0 ? "양수" : (num < 0 ? "음수" : "0"));
+	}
+}
+
+class Test
+{
+	public static void main(String[] args)
+	{
+		int num = 456;
+		System.out.println(num/100*100);
+	}
+}
+
+class Test
+{
+	public static void main(String[] args)
+	{
+		int num = 333;
+		System.out.println(num/10*10+1);
+	}
+}
+
+class Test
+{
+	public static void main(String[] args)
+	{
+		int num = 24;
+		System.out.println(10-num%10);
+	}
+}
+
+class Test
+{
+	public static void main(String[] args)	
+	{
+		int fahrenheit = 100;
+		float celcius = (int)((5/9F*(fahrenheit-32))*100+0.5)/100F;
+
+		System.out.println("Fahrenheit : "+fahrenheit);
+		System.out.println("Celcius : "+celcius);
+	}
+}
+*/
+class Test
+{
+	public static void main(String[] args)
+	{
+		char ch = 'A';
+
+		char lowerCase = (ch >= 'A' && ch <= 'Z') ? (char)(ch+32) : ch;
+
+		System.out.println("ch : " + ch);
+		System.out.println("ch to lowerCase : " + lowerCase);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
