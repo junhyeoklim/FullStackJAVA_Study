40e303931f04ba9ac5c3641f73115e852647d7d7
BuBu <100755527+junhyeoklim@users.noreply.github.com>
2024-03-12 14:28:19 +0900

    Delete Day22/Test directory

--- Day22/Test/src/day21/MultiImple.java
+++
@@ -1,9 +0,0 @@
-package day21;
-
-public class MultiImple implements Nice {
-
-	public MultiImple() {
-		// TODO Auto-generated constructor stub
-	}
-
-}
--- Day22/Test/src/day21/Nice.java
+++
@@ -1,5 +0,0 @@
-package day21;
-
-public interface Nice {
-
-}
