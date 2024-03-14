5c60843a11084ee4c5553b6cba4c29d0ba1e1345
junhyeoklim <gur710@gmail.com>
2024-03-14 17:22:48 +0900

    내용 수정

--- Day23/VectorTest/src/chapter07/Exercise7_1.java
+++ Day23/VectorTest/src/chapter07/Exercise7_1.java
@@ -108,22 +108,17 @@ package chapter07;
 //
 //class Marine extends Unit { // 보병
 //	int x, y; // 현재 위치
-//	void move(int x, int y) {
-//		super.move(x,y);
-//	}  // 지정된 위치로 이동
-//	public void stop() {  }// 현재 위치에 정지
+//	void move(int x, int y) {  }  // 지정된 위치로 이동
 //	void stimPack() {}// 스팀팩을 사용한다
 //}
 //class Tank { // 탱크
 //	int x, y; // 현재 위치
-//	public void move(int x, int y) {}  // 지정된 위치로 이동
-//	public void stop() {  }// 현재 위치에 정지
+//	public void move(int x, int y) {}  // 지정된 위치로 이동//	
 //	void changeMode() { /* . */} //공격모드를 변환한다
 //}
 //class Dropship { // 수송선
 //	int x, y; // 현재 위치
 //	public void move(int x, int y) {}  // 지정된 위치로 이동
-//	public void stop() {  }// 현재 위치에 정지
 //	void load() { /* .*/ } //선택된 대상을 태운다
 //	void unload() { /* .*/ } //선택된 대상을 내린다
 //}
