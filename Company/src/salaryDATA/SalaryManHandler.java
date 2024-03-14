4c116c2f6ec88d648119f42927350ece5f87f605
junhyeoklim <gur710@gmail.com>
2024-03-14 18:14:49 +0900

    SalaryManHandler 기능 수정

--- Company/src/salaryDATA/SalaryManHandler.java
+++ Company/src/salaryDATA/SalaryManHandler.java
@@ -7,10 +7,8 @@ import java.util.regex.Pattern;
 public class SalaryManHandler {
 
 	private Vector<SalaryManInfo> man;
-	private int cnt;
-	//	private static SalaryManHandler sms;
 	private static SalaryManHandler sms;	
-
+	private boolean check;
 	//싱글톤 사용이유 : 검색 하거나 데이터 입력을 할때마다 새로운 객체가 생성 되는지 배열에 널값이 들어감
 	private SalaryManHandler(int size)
 	{
@@ -64,6 +62,7 @@ public class SalaryManHandler {
 	public int search(String name)
 	{
 		int manLength = man.size();
+		
 		//기존에 사용하던 알고리즘
 //		for (int i = 0; i < manLength; i++) {
 //			if(man.get(i).getName().compareTo(name) == 0)
@@ -86,95 +85,80 @@ public class SalaryManHandler {
 	public void searchDepartment(String department)
 	{   
 		int manLength = man.size();
-		try 
-		{
+		
 			//입력한 값에 해당 되는 값을 전부 다 찾아서 출력하는 반복문
-			for(int i =0;i<manLength;i++)
-			{
-				if(department.equals(man.get(i).getDepartment()))
+		Pattern p = Pattern.compile(department);
+
+		for (int i = 0; i < manLength; i++) {			
+			Matcher m = p.matcher(man.get(i).getDepartment());
+				if(m.matches())
 				{
 					//조건이 참이면 입력한 부서에 해당 되는 데이터를 출력
 					System.out.println();
-					man.get(i).showManDepartment();		
-				}
-				else if( !(department.equals(man.get(i).getDepartment())) && i == manLength)		
-				{					
-					break;
-				}
-			}	
-		}	
-		catch (NullPointerException e) {
-
-		}
-
+					man.get(i).showManDepartment();	
+					check = true;
+				}				
+			}
+		if(!check)
+			System.out.println("찾으시는 사용자 정보가 없습니다.");
 	}
 
 	public void searchRank(String rank)
 	{
 		int manLength = man.size();
-		try 
-		{
-			//입력한 값에 해당 되는 값을 전부 다 찾아서 출력하는 반복문
-			for(int i =0;i<manLength;i++)
-			{
-				if(rank.equals(man.get(i).getRank()))
-				{
-					//조건이 참이면 입력한 직급에 해당 되는 데이터를 출력
-					man.get(i).showManRank();				
-				}
-				else if( !(rank.equals(man.get(i).getRank())) && i == manLength)		
-				{					
-					break;
-				}
-			}	
-		}	
-		catch (NullPointerException e) {
+		Pattern p = Pattern.compile(rank);
 
-		}
+		for (int i = 0; i < manLength; i++) {			
+			Matcher m = p.matcher(man.get(i).getRank());
+				if(m.matches())
+				{
+					//조건이 참이면 입력한 부서에 해당 되는 데이터를 출력
+					System.out.println();
+					man.get(i).showManRank();	
+					check = true;
+				}				
+			}
+		if(!check)
+			System.out.println("찾으시는 사용자 정보가 없습니다.");
 
 	}
 	public void searchSalary(String salary)
 	{
 		int manLength = man.size();
-		try 
-		{
-			//입력한 값에 해당 되는 값을 전부 다 찾아서 출력하는 반복문
-			for(int i=0;i<manLength;i++)
-			{
-				if(salary.equals(man.get(i).getSalary()))
-				{
-					//조건이 참이면 입력한 연봉에 해당 되는 데이터를 출력
-					man.get(i).showManSalary();
-				}
-				else if( !(salary.equals(man.get(i).getRank())) && i == manLength)		
-				{					
-					break;
-				}
-			}
-		}		
-		catch (NullPointerException e) {
+		Pattern p = Pattern.compile(salary);
 
-		}
+		for (int i = 0; i < manLength; i++) {			
+			Matcher m = p.matcher(man.get(i).getSalary());
+				if(m.matches())
+				{
+					//조건이 참이면 입력한 부서에 해당 되는 데이터를 출력
+					System.out.println();
+					man.get(i).showManSalary();	
+					check = true;
+				}				
+			}
+		if(!check)
+			System.out.println("찾으시는 사용자 정보가 없습니다.");
 	}
 	//등록된 사원 전부를 출력하는 메소드
 	public void salaryManeList()
 	{
 		int manLength = man.size();
-		try 
+		
+		if(manLength == 0)
 		{
-			for(int i =0;i<manLength;i++)
-			{		
-				if(man.get(i).equals(null))									
-					break;
-
-				man.get(i).showManAllInfo();
-				System.out.println();
-			}
-
-		}	
-		catch (NullPointerException e) {
-
+			System.out.println("사원 정보가 존재하지 않습니다.\n");
+			return;
 		}
+
+			for(int i =0;i<manLength;i++)
+			{
+				man.get(i).showManAllInfo();
+				System.out.println();			
+
+			}	
+			
+
 	}
 
 }
