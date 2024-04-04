9f7dbbb5d74ecd4035558026299afd46e72037d7
junhyeoklim <gur710@gmail.com>
2024-04-04 16:53:49 +0900

    39일차 수업내용 정리

---
+++ Day39/src/optional/StreamTestQuestion.java
@@ -0,0 +1,37 @@
+package optional;
+
+import java.util.Arrays;
+
+public class StreamTestQuestion {
+
+	public static void main(String[] args) {
+		
+		
+//		1번 문제
+//		int[] ar = {1, 2, 3, 4, 5};
+//		
+//		int sum = Arrays.stream(ar)
+//				   .filter(n -> n%2 == 1)
+//				   .sum();
+//		
+//		System.out.println(sum);
+		
+//		2번 문제
+//		String[] names = {"YOON", "LEE", "PARK"};
+//		
+//		Arrays.stream(names)
+//			   .forEach(s -> System.out.println(s));
+
+//		3번 문제
+//		double[] ds = {1.1, 2.2, 3.3, 4.4, 5.5};
+//		
+//		Arrays.stream(ds)
+//			  .forEach(d -> System.out.println(d));
+//		
+//		Arrays.stream(ds,1,4)
+//		.forEach(d -> System.out.println(d));
+		
+//		4번 문제
+	}
+
+}
